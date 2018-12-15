e3364fc0f8bc11d3eb7317c1851df61b37362d91
Inga Pflaumer <inga.pflaumer@gmail.com>
2018-12-15 17:59:23 +1100

    Lesson 1

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFirstFunction(){
+  console.log("myFirstFunction called");
+  let paragraph = document.getElementById("appearingP");
+  console.log(paragraph);
+  paragraph.style.display = "block";
+}
+
+//myFirstFunction()
